ec077ab76511a46a1c59c92cb67a464a7dcdfbba
raulsteleac <raulsteleac@yahoo.com>
2019-12-17 00:24:23 +0200

    Profile layout version 2., edit name + list

---
+++ app/src/main/java/org/redstudios/objecthunt/ObjectsAdapter.java
@@ -0,0 +1,57 @@
+package org.redstudios.objecthunt;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ObjectsAdapter extends ArrayAdapter<Pair<String, String>> {
+
+private Context context;
+private List<Pair<String,String>> obj_count;
+private int layoutResID;
+
+public ObjectsAdapter(Context context, int layoutResourceID, List<Pair<String, String>> obj_count) {
+        super(context, layoutResourceID, obj_count);
+        this.context = context;
+        this.obj_count = obj_count;
+        this.layoutResID = layoutResourceID;
+        }
+
+    static class ObjViewHolder {
+        public TextView objName;
+        public TextView objCount;
+    }
+@Override
+public View getView(int position, View convertView, ViewGroup parent) {
+    ObjViewHolder holder;
+    View view = convertView;
+
+    if (view == null) {
+    holder = new ObjViewHolder();
+    LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+
+    view = inflater.inflate(layoutResID, parent, false);
+    holder.objName = (TextView) view.findViewById(R.id.list_item_obj);
+    holder.objCount = (TextView) view.findViewById(R.id.list_item_count);
+
+    view.setTag(holder);
+    } else {
+        holder = (ObjViewHolder) view.getTag();
+    }
+
+    Pair<String,String> item = obj_count.get(position);
+
+    holder.objName.setText(item.first);
+    holder.objCount.setText(item.second);
+
+    return view;
+    }
+}
--- app/src/main/java/org/redstudios/objecthunt/ProfileFragment.java
+++ app/src/main/java/org/redstudios/objecthunt/ProfileFragment.java
@@ -1,11 +1,23 @@
 package org.redstudios.objecthunt;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Pair;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -17,25 +29,84 @@ import java.util.List;
 public class ProfileFragment extends Fragment {
 
     ListView listScores;
+    ImageButton editButton;
+    TextView name;
+    EditText editName;
+    ViewSwitcher switcher;
+    View view;
+    FrameLayout touchInterceptor;
+
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.profile_fragment, container, false);
+        view = inflater.inflate(R.layout.profile_fragment, container, false);
         return view;
     }
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        ListView l2 = (ListView) getActivity().findViewById(R.id.TopObjList);
-        List<String> arr2 = new ArrayList<>();
-        arr2.add("Masina");
-        arr2.add("Telefon");
-        arr2.add("Casca");
-        arr2.add("Mouse");
-        arr2.add("Monitor");
-        ArrayAdapter<String> l2Adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, arr2);
-        l2.setAdapter(l2Adapter);
+        listScores = (ListView) getActivity().findViewById(R.id.TopObjList);
+        editButton = (ImageButton) getActivity().findViewById(R.id.EditNameButton);
+        name = (TextView) getActivity().findViewById(R.id.TextViewName);
+        editName = (EditText) getActivity().findViewById(R.id.hiddenEdiTName);
+        switcher = (ViewSwitcher) getActivity().findViewById(R.id.nameSwitcher);
+        touchInterceptor = (FrameLayout) getActivity().findViewById(R.id.touchInterceptor);
 
+        List<Pair<String,String>> arr2 = new ArrayList<>();
+        arr2.add(Pair.create("Masina", "300"));
+        arr2.add(Pair.create("Telefon", "200"));
+        arr2.add(Pair.create("Casca", "100"));
+        arr2.add(Pair.create("Mouse", "420"));
+        arr2.add(Pair.create("Monitor", "690"));
+        ObjectsAdapter l2Adapter = new ObjectsAdapter(getActivity(), R.layout.two_column_item_list, arr2);
+        listScores.setAdapter(l2Adapter);
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switcher.showNext();
+                editName.requestFocus();
+            }
+        });
+
+        editName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!hasFocus) {
+                    name.setText(editName.getText());
+                    switcher.showPrevious();
+                    hideKeyboardFrom(view);
+                }
+                else
+                    appearKeyboardFrom(view);
+            }
+        });
+
+        touchInterceptor.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if (editName.isFocused()) {
+                        Rect outRect = new Rect();
+                        editName.getGlobalVisibleRect(outRect);
+                        if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                            editName.clearFocus();
+                        }
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+    public void hideKeyboardFrom(View view) {
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+    public void appearKeyboardFrom(View view) {
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
     }
 }
