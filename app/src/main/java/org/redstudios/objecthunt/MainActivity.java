a30fb0775f310f5d8d082e372607a9a3bbcbde8a
Andrei <andreirosu97@gmail.com>
2019-12-18 22:52:43 +0200

    Moved onNavigatioItemSelected to a separate class.

--- app/src/main/java/org/redstudios/objecthunt/MainActivity.java
+++ app/src/main/java/org/redstudios/objecthunt/MainActivity.java
@@ -11,11 +11,10 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.redstudios.objecthunt.listeners.MenuItemSelectedListener;
+
 public class MainActivity extends AppCompatActivity {
 
-    int opp_enter_anim = 0;
-    int opp_exit_anim = 0;
-    String currentTag = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,33 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.nav_home);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId()){
-                    case R.id.nav_profile:
-                        if(currentTag != item.getTitle()) {
-                            opp_enter_anim = R.anim.enter_to_right;
-                            opp_exit_anim = R.anim.exit_to_left;
-                            MainActivity.this.navigateTo(new ProfileFragment(), (String)item.getTitle(), R.anim.enter_to_left, R.anim.exit_to_right);
-                        }
-                        break;
-                    case R.id.nav_home:
-                        if(currentTag!=  item.getTitle()) {
-                            MainActivity.this.navigateTo(new GameModeSelectFragment(), (String)item.getTitle(), opp_enter_anim, opp_exit_anim);
-                        }
-                        break;
-                    case R.id.nav_leader:
-                        if(currentTag !=  item.getTitle()) {
-                            opp_enter_anim = R.anim.enter_to_left;
-                            opp_exit_anim = R.anim.exit_to_right;
-                            MainActivity.this.navigateTo(new LeaderboardFragment(), (String)item.getTitle(), R.anim.enter_to_right, R.anim.exit_to_left);
-                        }
-                        break;
-                }
-                return true;
-            }
-        });
+        bottomNavigationView.setOnNavigationItemSelectedListener(new MenuItemSelectedListener(this));
     }
 
     public void navigateTo(Fragment fragment, String tag, int enter_anim, int exit_anim) {
@@ -64,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         transaction.setCustomAnimations(enter_anim, exit_anim,enter_anim, exit_anim);
         transaction.replace(R.id.fragment_container, fragment, tag);
         transaction.commit();
-        currentTag = tag;
     }
 }
 
---
+++ app/src/main/java/org/redstudios/objecthunt/listeners/MenuItemSelectedListener.java
@@ -0,0 +1,51 @@
+package org.redstudios.objecthunt.listeners;
+
+
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.redstudios.objecthunt.GameModeSelectFragment;
+import org.redstudios.objecthunt.LeaderboardFragment;
+import org.redstudios.objecthunt.MainActivity;
+import org.redstudios.objecthunt.ProfileFragment;
+import org.redstudios.objecthunt.R;
+
+import androidx.annotation.NonNull;
+
+public class MenuItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    public MenuItemSelectedListener(MainActivity caller_) {
+        caller = caller_;
+    }
+
+    MainActivity caller;
+    int lastItemSelected = -1;
+
+    int opp_enter_anim = 0;
+    int opp_exit_anim = 0;
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (lastItemSelected == item.getItemId()) {
+            return true;
+        }
+        switch (item.getItemId()) {
+            case R.id.nav_profile:
+                opp_enter_anim = R.anim.enter_to_right;
+                opp_exit_anim = R.anim.exit_to_left;
+                caller.navigateTo(new ProfileFragment(), (String) item.getTitle(), R.anim.enter_to_left, R.anim.exit_to_right);
+                break;
+            case R.id.nav_home:
+                caller.navigateTo(new GameModeSelectFragment(), (String) item.getTitle(), opp_enter_anim, opp_exit_anim);
+                break;
+            case R.id.nav_leader:
+                opp_enter_anim = R.anim.enter_to_left;
+                opp_exit_anim = R.anim.exit_to_right;
+                caller.navigateTo(new LeaderboardFragment(), (String) item.getTitle(), R.anim.enter_to_right, R.anim.exit_to_left);
+                break;
+        }
+        lastItemSelected = item.getItemId();
+        return true;
+    }
+}
