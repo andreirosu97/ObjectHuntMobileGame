1d8b2b4598d7009429e8bec5cf33fdfc83585eb9
Andrei <andreirosu97@gmail.com>
2020-01-01 17:47:09 +0200

    BETA v0.3 Challenge button functionality

--- app/src/main/java/org/redstudios/objecthunt/GameOverActivity.java
+++ app/src/main/java/org/redstudios/objecthunt/GameOverActivity.java
@@ -1,7 +1,7 @@
 package org.redstudios.objecthunt;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -28,7 +28,6 @@ public class GameOverActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        Log.d("RAUL", "On create");
         super.onCreate(savedInstanceState);
 
         Bundle gameResult = getIntent().getExtras();
@@ -72,5 +71,18 @@ public class GameOverActivity extends AppCompatActivity {
                     finish();
                 }
         );
+
+        challengeButton.setOnClickListener((View view) -> {
+            try {
+                String shareBody = "Hey, I challenge you to beat by score of " + topPoints + " points in the " + gameMode + " mode. Here is the app check it out : <link to app>";
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "I challenge you !");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share via"));
+            } catch (Exception e) {
+                //e.toString();
+            }
+        });
     }
 }
